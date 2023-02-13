51a54a2b517a9e1629086463ab1c35085c71b91d
OlgaP11 <ooolchaaa@inbox.ru>
2023-02-13 15:51:46 +0300

    Add second solution task 4

--- Homework_1/task_4.java
+++ Homework_1/task_4.java
@@ -4,9 +4,9 @@ public class task_4 {
     public static void main(String [] args) {
         System.out.println("Восстановление уравнения типа q + w = e, где q, w, e >= 0.");
         Scanner scan = new Scanner (System.in);   
-        System.out.print("Введите первое число: ");     
+        System.out.print("Введите первое число (вместо неизвестной цифры '?'): ");     
         String s1 = scan.nextLine();
-        System.out.print("Введите второе число: ");     
+        System.out.print("Введите второе число (вместо неизвестной цифры '?'): ");     
         String s2 = scan.nextLine();
         System.out.print("Введите результат: ");
         int result = scan.nextInt();
@@ -15,9 +15,11 @@ public class task_4 {
         int num2 = getNums(s2);
         int answer = result - num1 - num2;
         if (answer >=0){
-            num1 = Math.max(num1,num2)+answer%10;
-            num2 = Math.min(num1,num2)+(answer - answer%10);
-            System.out.printf("первоначальный вид: %d + %d = %d", num1, num2, result);  
+            int minNum = Math.min(num1,num2);
+            int maxNum = Math.max(num1,num2);
+            num1 = maxNum+answer%10;
+            num2 = minNum+(answer - answer%10);
+            System.out.printf("Первоначальный вид: %d + %d = %d", num1, num2, result);  
         } else{
             System.out.println("Нет решения.");
         }
@@ -29,11 +31,13 @@ public class task_4 {
             digits *=10;
         }
         int num = 0;
-        for (int i = 0; i < s.length();i++){
-            if (Character.isDigit(s.charAt(i))){
-                num = Character.digit(s.charAt(i), 10)*digits;
-            } 
-            digits/=10;
+        if (s.length() > 1){
+            for (int i = 0; i < s.length();i++){
+                if (Character.isDigit(s.charAt(i))){
+                    num += Character.digit(s.charAt(i), 10)*digits;
+                } 
+                digits/=10;
+            }
         }
         return num;  
     }
